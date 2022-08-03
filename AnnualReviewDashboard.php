4241cdc657a1940f4a9a700ed9347a1af8e86026
AndrewPoppe <AndrewBPoppe@gmail.com>
2022-08-03 16:22:27 -0400

    working

--- AnnualReviewDashboard.php
+++ AnnualReviewDashboard.php
@@ -87,6 +87,96 @@ class AnnualReviewDashboard extends \ExternalModules\AbstractExternalModule {
         }
     }
 
+    function getStatus($id, $record) {
+        // 0 - pending faculty submission (initial form completed only)
+        // 1 - pending mentor review
+        // 2 - pending division chief review
+        // 3 - pending chair review
+        // 4 - ready for mentor review (show first stage link)
+        // 5 - ready for division chief review (show first stage link)
+        // 6 - ready for final review (show final link)
+        // 7 - final review completed
+        $chair_completed = $record["chairs_comments_for_faculty_development_annual_que_complete"] == 2;
+        $faculty_completed = $record["faculty_development_annual_questionnaire_2022_complete"] == 2;
+        $first_stage_complete = $record["first_stage_review_comments_for_faculty_developmen_complete"] == 2;
+        $review_type = $record["review_type"];
+        $mentor = $record["mentor_name"];
+        $division_chief = $record["division_chief_name"];
+        $chair = $record["departmental_leadership"];
+
+        $userIsMentor = $mentor == $id;
+        $userIsDivisionChief = $division_chief == $id;
+        $userIsChair = $chair == $id;
+
+        $status = 0;
+        if ($chair_completed) {
+            $status = 7;
+        } else if (!$faculty_completed) {
+            $status = 0;
+        } else if (($first_stage_complete && $userIsChair) || ($review_type == 1 && $userIsChair)) {
+            $status = 6;
+        } else if ($review_type == 3 && $userIsDivisionChief && !$first_stage_complete) {
+            $status = 5;
+        } else if ($review_type == 4 && $userIsMentor && !$first_stage_complete) {
+            $status = 4;
+        } else if ($first_stage_complete || $review_type == 1) {
+            $status = 3;
+        } else if ($review_type == 3) {
+            $status = 2;
+        } else if ($review_type == 4) {
+            $status = 1;
+        }
+        return $status;
+    }
+
+    function getStatusText($status) {
+        // 0 - pending faculty submission (initial form completed only)
+        // 1 - pending mentor review
+        // 2 - pending division chief review
+        // 3 - pending chair review
+        // 4 - ready for mentor review (show first stage link)
+        // 5 - ready for division chief review (show first stage link)
+        // 6 - ready for final review (show final link)
+        // 7 - final review completed
+        switch (strval($status)) {
+            case "1":
+                $status_text = "Pending Mentor Review";
+                break;
+            case "2":
+                $status_text = "Pending Division Chief Review";
+                break;
+            case "3":
+                $status_text = "Pending Chair Review";
+                break;
+            case "4":
+                $status_text = "Ready for First Review";
+                break;
+            case "5":
+                $status_text = "Ready for First Review";
+                break;
+            case "6":
+                $status_text = "Ready for Final Review";
+                break;
+            case "7":
+                $status_text = "Final Review Completed";
+                break;
+            default:
+                $status_text = "Pending Faculty Submission";
+                break;
+        }
+        return $status_text;
+    }
+
+    function getLink($record) {
+        $link = "";
+        if ($record["status"] == 6) {
+            $link = $record["link_to_faculty_survey"];
+        } else if ($record["status"] == 4 || $record["status"] == 5) {
+            $link = $record["link_to_faculty_survey_first_stage"];
+        }
+        return $link;
+    }
+
     function getData($id) {
         $params = array(
             "project_id" => $project_id,
@@ -96,21 +186,33 @@ class AnnualReviewDashboard extends \ExternalModules\AbstractExternalModule {
                 "init_department",
                 "init_ladder_track",
                 "init_rank",
-                "link_to_faculty_survey"
+                "review_type",
+                "mentor_name",
+                "division_chief_name",
+                "departmental_leadership",
+                "faculty_development_annual_questionnaire_2022_complete",
+                "first_stage_review_comments_for_faculty_developmen_complete",
+                "chairs_comments_for_faculty_development_annual_que_complete",
+                "link_to_faculty_survey",
+                "link_to_faculty_survey_first_stage"
             ),
-            "filterLogic" => '([departmental_leadership] = "'.$id.'") AND ([faculty_development_annual_questionnaire_2022_complete] = "2") AND ([chairs_comments_for_faculty_development_annual_que_complete] = "0")',
+            //"filterLogic" => '([departmental_leadership] = "'.$id.'") AND ([faculty_development_annual_questionnaire_2022_complete] = "2") AND ([chairs_comments_for_faculty_development_annual_que_complete] = "0")',
+            "filterLogic" => '([departmental_leadership] = "'.$id.'") OR ([mentor_name] = "'.$id.'") OR ([division_chief_name] = "'.$id.'")',
             "exportAsLabels" => "TRUE"        
         );
         $data = \REDCap::getData($params);
         foreach ($data as &$record) {
-            $event = $record[$this->getEventId()];
+            $record = $record[$this->getEventId()];
             
-            $record["init_first_name"] = $event["init_first_name"] ;
-            $record["init_last_name"] = $event["init_last_name"];
-            $record["init_department"] = $this->getChoiceLabel("init_department", $event["init_department"]);
-            $record["init_ladder_track"] = $this->getChoiceLabel("init_ladder_track", $event["init_ladder_track"]);
-            $record["init_rank"] = $this->getChoiceLabel("init_rank", $event["init_rank"]);
-            $record["link_to_faculty_survey"] = $event["link_to_faculty_survey"];
+            //$record["init_first_name"] = $event["init_first_name"] ;
+            //$record["init_last_name"] = $event["init_last_name"];
+            $record["init_department"] = $this->getChoiceLabel("init_department", $record["init_department"]);
+            $record["init_ladder_track"] = $this->getChoiceLabel("init_ladder_track", $record["init_ladder_track"]);
+            $record["init_rank"] = $this->getChoiceLabel("init_rank", $record["init_rank"]);
+            $record["status"] = $this->getStatus($id, $record);
+            $record["link"] = $this->getLink($record);
+            $record["status_text"] = $this->getStatusText($record["status"]);
+            //$record["link_to_faculty_survey"] = $event["link_to_faculty_survey"];
         }
         return $data;
     }
@@ -121,15 +223,18 @@ class AnnualReviewDashboard extends \ExternalModules\AbstractExternalModule {
             return;
         }
         ?>
-        <table>
-            <tr>
+        <div style="width:75%;">
+        <table id="dashboard_table">
+            <thead><tr>
                 <th>First Name</th>
                 <th>Last Name</th>
                 <th>Department</th>
                 <th>Ladder Track</th>
                 <th>Rank</th>
+                <th>Review Type</th>
+                <th>Status</th>
                 <th>Survey Link</th>
-            </tr>
+            </tr></thead>
             <?php foreach ($data as $record) { ?>
 
                 <tr>
@@ -138,13 +243,25 @@ class AnnualReviewDashboard extends \ExternalModules\AbstractExternalModule {
                     <td><?=\REDCap::escapeHtml($record["init_department"])?></td>
                     <td><?=\REDCap::escapeHtml($record["init_ladder_track"])?></td>
                     <td><?=\REDCap::escapeHtml($record["init_rank"])?></td>
-                    <td><a href="<?=\REDCap::escapeHtml($record["link_to_faculty_survey"])?>">link</a></td>
+                    <td><?=\REDCap::escapeHtml($this->getChoiceLabel('review_type', $record["review_type"]))?></td>
+                    <td><?=\REDCap::escapeHtml($record["status_text"])?></td>
+                    <td>
+                        <?php if ($record["link"] !== "") { ?>
+                            <a target="_blank" href="<?=\REDCap::escapeHtml($record["link"])?>">link</a>
+                        <?php } ?>
+                    </td>
                 </tr>
                 
             <?php } ?>
         </table>
+        </div>
+        <script>
+            $(document).ready( function () {
+                $('#dashboard_table').DataTable();
+            } );
+        </script>
         <?php
-        var_dump($data);
+        //var_dump($data);
     }
 
 }
--- dashboard.php
+++ dashboard.php
@@ -8,6 +8,20 @@ if ($id == FALSE) {
     //exit;
 }
 
+?>
+<script
+    src="https://code.jquery.com/jquery-3.6.0.slim.min.js"
+    integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI="
+    crossorigin="anonymous">
+</script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">  
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+
+<?php
+
 $data = $module->getData($id);
 $module->displayDataTable($data);
+
+?>
+
 
